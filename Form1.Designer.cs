6634214f90482ca7aba34e01fdc5bb825c613035
Mila <milaivanova2609@gmail.com>
2022-10-02 22:09:29 +0300

    Almost

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsForms
 {
-    partial class Form1
+    partial class LabForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,19 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.chooseDateButton = new System.Windows.Forms.Button();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
-            // Form1
+            // chooseDateButton
+            // 
+            this.chooseDateButton.Location = new System.Drawing.Point(96, 36);
+            this.chooseDateButton.Name = "chooseDateButton";
+            this.chooseDateButton.Size = new System.Drawing.Size(219, 29);
+            this.chooseDateButton.TabIndex = 0;
+            this.chooseDateButton.Text = "Choose a date";
+            this.chooseDateButton.UseVisualStyleBackColor = true;
+            this.chooseDateButton.Click += new System.EventHandler(this.chooseDateButton_Click);
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(113, 85);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(0, 15);
+            this.dateLabel.TabIndex = 1;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(96, 121);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(219, 23);
+            this.dateTimePicker.TabIndex = 2;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // LabForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(407, 192);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.chooseDateButton);
+            this.Name = "LabForm";
+            this.Text = "Choosing a date";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button chooseDateButton;
+        private Label dateLabel;
+        private DateTimePicker dateTimePicker;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,21 @@
+
 namespace WindowsForms
 {
-    public partial class Form1 : Form
+    public partial class LabForm : Form
     {
-        public Form1()
+        public LabForm()
         {
             InitializeComponent();
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            dateLabel.Text = String.Format("You entered: {0}", dateTimePicker.Value.ToString());
+        }
+
+        private void chooseDateButton_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
